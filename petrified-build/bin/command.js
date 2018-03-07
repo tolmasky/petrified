9e8e5438dd99c318bc3852b0ae6628e4a7625654
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2018-03-07 10:52:01 -0800

    Clean up commands.
    Reviewed by @tolmasky.

---
+++ petrified-build/bin/command.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+
+const node = process.versions.node;
+const babelOptions = {
+    presets: [["@isomorphic/babel-preset", { node, react:true }]],
+    plugins: ["babel-plugin-emotion"]
+};
+
+module.exports = function (commander, name)
+{
+    const command = name ?
+        commander.command(`${name} [source]`) : commander;
+
+    return command
+        .option("-o, --output [output]", "Output destination")
+        .option("--cache [cache]", "Cache destination") 
+        .option("--drafts", "Include drafts")
+        .action(function (mSource, options)
+        {
+            const cwd = process.cwd();
+            const resolve = filename => path.resolve(cwd, filename);
+
+            const source = resolve(mSource || ".");
+            const destination = resolve(options.output || `${source}/_site`);
+            const cache = resolve(options.cache || `${source}/_cache`);
+            const drafts = !!options.drafts;
+            const site = require(`${source}/petrified.json`);
+
+            require("magic-ws/babel-register")([{ source, options: babelOptions }]);
+            require("../petrified-build")({ site, drafts, source, destination, cache });
+        })
+}
